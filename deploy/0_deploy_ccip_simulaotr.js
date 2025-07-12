362b4946b5dbec3e02ba4bb6716749fdb4975595
ruochen <wangrui@ruochen.email>
2025-07-12 17:52:03 +0800

    ccip deploy

---
+++ deploy/0_deploy_ccip_simulaotr.js
@@ -0,0 +1,15 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { firstAccount } = await getNamedAccounts()
+    const { deploy, log } = deployments
+
+    log("Deploying CCIP Simulator contract")
+    await deploy("CCIPLocalSimulator", {
+        contract: "CCIPLocalSimulator",
+        from: firstAccount,
+        log: true,
+        args: []
+    })
+    log("CCIP Simulator contract deployed successfully")
+}
+
+module.exports.tags = ["test", "all"]
---
+++ deploy/4_deploy_pool_burn_and_mint.js
@@ -0,0 +1,25 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { firstAccount } = await getNamedAccounts()
+    const { deploy, log } = deployments
+
+    log("NFTPoolBurnAndMint deploying...")
+
+    const ccipSimulatorDeployment = await deployments.get("CCIPLocalSimulator")
+    const ccipSimulator = await ethers.getContractAt("CCIPLocalSimulator", ccipSimulatorDeployment.address)
+    const ccipConfig = await ccipSimulator.configuration()
+    const destChainRouter = ccipConfig.destinationRouter_
+    const linkTokenAddr = ccipConfig.linkToken_
+    const wnftDeployment = await deployments.get("WrappedMyToken")
+    const wnftAddr = wnftDeployment.address
+
+    await deploy("NFTPoolBurnAndMint", {
+        contract: "NFTPoolBurnAndMint",
+        from: firstAccount,
+        log: true,
+        args: [destChainRouter, linkTokenAddr, wnftAddr]
+    })
+
+    log("NFTPoolBurnAndMint deployed successfully")
+}
+
+module.exports.tags = ["destChain", "all"]
